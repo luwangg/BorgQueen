1b42bc905e7bda4266151686410739cf393023bd
Tom Bottiglieri <tom.bottiglieri@gmail.com>
2012-12-03 22:53:51 -0800

    Add a safety watchdog to the drone controller

--- drone/fly.js
+++ drone/fly.js
@@ -14,34 +14,60 @@ function handler (req, res) {
       res.writeHead(500);
       return res.end('Error loading index.html');
     }
-
     res.writeHead(200);
     res.end(data);
   });
 }
 
 function now() {
-  return Date.now()
+  return Date.now();
 }
+
+function land() {
+  client.stop();
+  client.land();
+}
+
+var state = {};
+var last_update = 0;
+var runnerId = 0;
 
 io.sockets.on('connection', function (socket) {
   socket.on('fly', function (data) {
-
-    client.right(data.x || 0);
-    client.front(data.y || 0);
-    client.up(data.z || 0);
-    client.clockwise(data.r || 0);
+    last_update = now();
+    state.x = (data.x || 0);
+    state.y = (data.y || 0);
+    state.z = (data.z || 0);
+    state.r = (data.r || 0);
 
     if (data.takeoff)
-      client.takeoff();
-    if (data.land) {
-      client.stop();
-      client.land();
-    }
+      startFlying();
+    if (data.land)
+      stopFlying();
   });
-
 });
 
+function startFlying() {
+  console.log("Starting drone");
+  client.takeoff();
+  runner = setInterval( function() {
+    if (now() - last_update > 250) {
+      stopFlying();
+    }
+    console.log(state);
+    client.right(state.x || 0);
+    client.front(state.y || 0);
+    client.up(state.z || 0);
+    client.clockwise(state.r || 0);
+  }, 50);
+}
+
+function stopFlying() {
+  console.log("Stopping drone");
+  clearInterval(runner);
+  state = {};
+  land();
+}
 
 process.on('SIGINT', function() {
   console.log("\nGracefully shutting down from SIGINT (Ctrl+C)");
